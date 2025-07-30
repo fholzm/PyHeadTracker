ae36a812683fcaa2d776edb47f3fe12bbac22adc
Felix Holzmüller <f.holzmueller@kug.ac.at>
2025-07-30 15:57:59 +0200

    pyopenxr isn't available on macOS, so don't provide hmd class there

--- src/pyheadtracker/__init__.py
+++ src/pyheadtracker/__init__.py
@@ -1,15 +1,30 @@
+import sys
+
 from . import supperware
 from . import iem
-from . import hmd
 from . import utils
 from .dtypes import Quaternion, YPR, Position
 
-__all__ = [
-    "supperware",
-    "iem",
-    "hmd",
-    "utils",
-    "Quaternion",
-    "YPR",
-    "Position",
-]
+# Don't import hmt if on macOS
+if sys.platform != "darwin":
+    from . import hmt
+
+    __all__ = [
+        "supperware",
+        "iem",
+        "hmd",
+        "utils",
+        "Quaternion",
+        "YPR",
+        "Position",
+    ]
+else:
+    __all__ = [
+        "supperware",
+        "iem",
+        # "hmd",
+        "utils",
+        "Quaternion",
+        "YPR",
+        "Position",
+    ]
