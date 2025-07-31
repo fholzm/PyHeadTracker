db161cd896a96c53213e262d0b499acfdbdd5e2c
Felix Holzmüller <f.holzmueller@kug.ac.at>
2025-07-31 11:13:08 +0200

    Fix OS-specific import

--- src/pyheadtracker/__init__.py
+++ src/pyheadtracker/__init__.py
@@ -1,30 +1,25 @@
-import sys
+# pyheadtracker/__init__.py
+
+import platform
+
+from .dtypes import Quaternion, YPR
+
+__all__ = ["Quaternion", "YPR"]
+
+# Conditionally import modules
+if platform.system() in ("Linux", "Windows"):
+    from . import hmd
+
+    __all__.append("hmd")
+else:
+    print(
+        "pyheadtracker: 'hmd' module is only available on Linux and Windows platforms."
+    )
 
 from . import supperware
 from . import iem
 from . import utils
-from .dtypes import Quaternion, YPR, Position
 
-# Don't import hmt if on macOS
-if sys.platform != "darwin":
-    from . import hmt
-
-    __all__ = [
-        "supperware",
-        "iem",
-        "hmd",
-        "utils",
-        "Quaternion",
-        "YPR",
-        "Position",
-    ]
-else:
-    __all__ = [
-        "supperware",
-        "iem",
-        # "hmd",
-        "utils",
-        "Quaternion",
-        "YPR",
-        "Position",
-    ]
+__all__.append("supperware")
+__all__.append("iem")
+__all__.append("utils")
