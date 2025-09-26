8eb20429d21fca17c364034ee211ec3a50be164b
Felix Holzmüller <f.holzmueller@kug.ac.at>
2025-09-26 11:53:49 +0200

    Add SPARTA output

--- src/pyheadtracker/out.py
+++ src/pyheadtracker/out.py
@@ -228,6 +228,104 @@ class IEMDirectivityShaper(OutBase):
 
 
 # TODO: Implement RoomEncoder
-# TODO: Implement SPARTA
+
+
+class SPARTA(OutBase):
+    """Class for sending head tracking data to SPARTA AmbiBin via OSC.
+
+    This class is used to transmit data via OSC to the SPARTA AmbiBin application [1]. It supports sending orientation data in both YPR (Yaw, Pitch, Roll) and Quaternion formats. If Quaternion data is provided, it is automatically converted to YPR format before transmission.
+
+    Attributes
+    ----------
+    ip : str
+        The IP address of the target application. Default is "127.0.0.1".
+    port : int
+        The port number of the target application. Default is 9000.
+    offset_az : float
+        Offset in degrees to be added to the azimuth (yaw) angle. Default is 0.0.
+    offset_el : float
+        Offset in degrees to be added to the elevation (pitch) angle. Default is 0.0.
+    offset_roll : float
+        Offset in degrees to be added to the roll angle. Default is 0.0.
+    invert_az : bool
+        If True, invert the azimuth (yaw) angle. Default is False.
+    invert_el : bool
+        If True, invert the elevation (pitch) angle. Default is False.
+    invert_roll : bool
+        If True, invert the roll angle. Default is False.
+    """
+
+    def __init__(
+        self,
+        ip: str = "127.0.0.1",
+        port: int = 9000,
+        offset_az: float = 0.0,
+        offset_el: float = 0.0,
+        offset_roll: float = 0.0,
+        invert_az: bool = False,
+        invert_el: bool = False,
+        invert_roll: bool = False,
+    ):
+        """
+        Parameters
+        ----------
+        ip : str
+            The IP address of the target application. Default is "127.0.0.1".
+        port : int
+            The port number of the target application. Default is 9000.
+        offset_az : float
+            Offset in degrees to be added to the azimuth (yaw) angle. Default is 0.0.
+        offset_el : float
+            Offset in degrees to be added to the elevation (pitch) angle. Default is 0.0.
+        offset_roll : float
+            Offset in degrees to be added to the roll angle. Default is 0.0.
+        invert_az : bool
+            If True, invert the azimuth (yaw) angle. Default is False.
+        invert_el : bool
+            If True, invert the elevation (pitch) angle. Default is False.
+        invert_roll : bool
+            If True, invert the roll angle. Default is False.
+        """
+        self.ip = ip
+        self.port = port
+        self.client = SimpleUDPClient(self.ip, self.port)
+        self.offset_az = offset_az
+        self.offset_el = offset_el
+        self.offset_roll = offset_roll
+        self.invert_az = invert_az
+        self.invert_el = invert_el
+        self.invert_roll = invert_roll
+
+    def send_orientation(self, orientation: YPR | Quaternion | None):
+        """Send orientation data to the IEM Scene Rotator.
+
+        Parameters
+        ----------
+        orientation : YPR | Quaternion
+            The orientation data to send.
+        """
+        if not isinstance(orientation, (YPR, Quaternion)):
+            return
+
+        if isinstance(orientation, Quaternion):
+            orientation = quat2ypr(orientation)
+
+        # TODO: Check for correct sign and range of angles
+        y, p, r = orientation.to_degrees()
+
+        y += self.offset_az
+        p += self.offset_el
+        r += self.offset_roll
+
+        if self.invert_az:
+            y = -y
+        if self.invert_el:
+            p = -p
+        if self.invert_roll:
+            r = -r
+
+        self.client.send_message("/ypr", [y, p, r])
+
+
 # TODO: Implement TASCAR
 # TODO: Implement generic OSC sender class?
